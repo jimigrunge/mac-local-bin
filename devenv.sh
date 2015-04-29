04c529f7e3ff83badc5f9916ad5ef6197dfdef7f
James Grundner <jgrundner@jfitzgeraldgroup.com>
2015-04-28 23:24:28 -0400

    Added more functions to server; switched postgres to use pg_ctl due to server locks; added myzsh config

--- devenv.sh
+++ devenv.sh
@@ -5,7 +5,7 @@
 # 
 # @author James Grundner <james@jgrundner.com>
 # v1.0 November 10, 2013
-# @version v3.1.3 March 12, 2015
+# @version v3.2 April 28, 2015
 # 
 ############################################################################
 #                User Definable                                            #
@@ -18,13 +18,24 @@
 HTTPPASSWORD="SET-TO-YOUR-PASSWORD"
 ARGV=$1
 APACHECTL="/usr/local/bin/apachectl"
+APACHE_ERROR_LOG=$(brew --prefix)/var/log/apache2/error_log
+APACHE_ACCESS_LOG=$(brew --prefix)/var/log/apache2/access_log
+DEV_ERROR_LOG="/Users/$USER/Sites/logs/dev-error_log"
+DEV_ACCESS_LOG="/Users/$USER/Sites/logs/dev-access_log"
 PHP_EXE="/usr/local/bin/php"
 PGSQL_PLIST="/Users/$USER/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
+PGSQL_DATA="/usr/local/var/postgres"
+PGSQL_LOGS="/usr/local/var/postgres/server.log"
+PGSQL_PID_FILE="/usr/local/var/postgres/postmaster.pid"
 MYSQL_PLIST="/Users/$USER/Library/LaunchAgents/homebrew.mxcl.mysql55.plist"
+MYSQL_LOGS=$(brew --prefix)/var/mysql/$(hostname).err
+MYSQL_PID=$(brew --prefix)/var/mysql/$(hostname).pid
+MYSQL_INIT_SCRIPT=mysql.server
 HTTPD_PLIST="/Users/$USER/Library/LaunchAgents/homebrew.mxcl.httpd22.plist"
 PHP53_PLIST="/Users/$USER/Library/LaunchAgents/homebrew.mxcl.php53.plist"
 PHP55_PLIST="/Users/$USER/Library/LaunchAgents/homebrew.mxcl.php55.plist"
 PHP56_PLIST="/Users/$USER/Library/LaunchAgents/homebrew.mxcl.php56.plist"
+PHP_FPM_LOG="/usr/local/var/log/php-fpm.log"
 ############################################################################
 #                End of User Definable                                     #
 ############################################################################
@@ -45,13 +56,13 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 # Define functions
 ############################################################################
 stop(){
-	server_stop
+	apache_stop
     mysql_stop
     postgres_stop
     RETVAL=$?
 }
 start(){
-	server_start
+	apache_start
     mysql_start
     postgres_start
     RETVAL=$?
@@ -61,49 +72,88 @@ restart(){
     start
 }
 
-server_start(){
+apache_start(){
 	echo "-> Starting Apache"
     launchctl load -Fw $HTTPD_PLIST
     sleep 1
 }
-server_stop(){
+apacher_stop(){
 	echo "-> Shutting down Apache"
     launchctl unload $HTTPD_PLIST
     sleep 1
 }
-server_restart(){
-	server_stop
-	server_start
+apache_restart(){
+	apache_stop
+	apache_start
 }
+apache_logs(){
+    echo "-> Apache error:  $APACHE_ERROR_LOG"
+    echo "-> Apache access: $APACHE_ACCESS_LOG"
+    echo "-> Dev error:     $DEV_ERROR_LOG"
+    echo "-> Dev access:    $DEV_ACCESS_LOG"
+}
+apache_error_log_stream(){
+    tail -f $APACHE_ERROR_LOG
+}
+apache_access_log_stream(){
+    tail -f $APACHE_ACCESS_LOG
+}
+apache_dev_error_stream(){
+    tail -f $DEV_ERROR_LOG
+}
+apache_dev_access_stream(){
+    tail -f $DEV_ACCESS_LOG
+}
+
 
 mysql_start(){
 	echo "-> Starting MySQL"
     launchctl load -Fw $MYSQL_PLIST
+    #$MYSQL_INIT_SCRIPT start
     sleep 1
 }
 mysql_stop(){
 	echo "-> Shutting down MySQL"
     launchctl unload $MYSQL_PLIST
+    #$MYSQL_INIT_SCRIPT stop
     sleep 1
 }
 mysql_restart(){
 	mysql_stop
 	mysql_start
 }
+mysql_log(){
+    echo "-> $MYSQL_LOGS"
+}
+mysql_log_stream(){
+    tail -f $MYSQL_LOGS
+}
+mysql_pid_file(){
+    echo "-> $MYSQL_PID"
+}
 
 postgres_start(){
     echo '-> Starting Postgresql'
-    launchctl load -Fw $PGSQL_PLIST
+    pg_ctl -D $PGSQL_DATA -l $PGSQL_LOGS start
     sleep 1
 }
 postgres_stop(){
     echo '-> Shutting down Postgresql'
-    launchctl unload $PGSQL_PLIST
+    pg_ctl $PGSQL_DATA stop -m fast
     sleep 1
 }
 postgres_restart(){
 	postgres_stop
 	postgres_start
+}
+postgres_log(){
+    echo echo "-> $PGSQL_LOGS"
+}
+postgres_log_stream(){
+    tail -f $PGSQL_LOGS
+}
+postgres_pid_file(){
+    echo "-> $PGSQL_PID_FILE"
 }
 
 load_php53(){
@@ -138,6 +188,14 @@ unload_php56(){
     launchctl unload $PHP56_PLIST
     sleep 1
 }
+
+php_logs(){
+    echo "-> $PHP_FPM_LOG"
+}
+php_log_stream(){
+    tail -f $PHP_FPM_LOG
+}
+
 end_session(){
     echo "-> Execution complete!"
 }
@@ -152,7 +210,7 @@ case "$ARGV" in
 		unload_php55
 		unload_php56
 		load_php53
-		server_restart
+		apache_restart
 		end_session
     ;;
     use55)
@@ -160,7 +218,7 @@ case "$ARGV" in
 		unload_php53
 		unload_php56
 		load_php55
-		server_restart
+		apache_restart
 		end_session
     ;;
     use56)
@@ -168,11 +226,20 @@ case "$ARGV" in
 		unload_php53
 		unload_php55
 		load_php56
-		server_restart
+		apache_restart
 		end_session
     ;;
     php)
-        $PHP_EXE -v
+        if [ -z $2 ] ; then
+            $PHP_EXE -v
+        else
+            case "$2" in
+                log)
+                    php_logs ;;
+                tail)
+                    php_log_stream ;;
+            esac 
+        fi
     ;;
     test)
         echo "$APACHECTL configtest"
@@ -181,11 +248,25 @@ case "$ARGV" in
     apache)
         case "$2" in
             stop)
-                server_stop ;;
+                apache_stop ;;
             start)
-                server_start ;;
+                apache_start ;;
             restart)
-                server_restart ;;
+                apache_restart ;;
+            logs)
+                apache_logs ;;
+            tail)
+                case "$3" in
+                    error)
+                        apache_error_log_stream ;;
+                    access)
+                        apache_access_log_stream ;;
+                    deverror)
+                        apache_dev_error_stream ;;
+                    devaccess)
+                        apache_dev_access_stream ;;
+                esac 
+            ;;
         esac
         end_session
     ;;
@@ -198,6 +279,12 @@ case "$ARGV" in
             restart)
                 postgres_stop
                 postgres_start ;;
+            log)
+                postgres_log ;;
+            tail)
+                postgres_log_stream ;;
+            pidfile)
+                postgres_pid_file ;;
         esac
         end_session
     ;;
@@ -210,23 +297,29 @@ case "$ARGV" in
             restart)
                 mysql_stop
                 mysql_start ;;
+            log)
+                mysql_log ;;
+            tail)
+                mysql_log_stream ;;
+            pidfile)
+                mysql_pid_file ;;
         esac
         end_session
     ;;
 	start)
-		server_start
+		apache_start
 		mysql_start
 		postgres_start
 		end_session
 	;;
 	stop)
-		server_stop
+		apache_stop
 		mysql_stop
 		postgres_stop
 		end_session
 	;;
 	restart)
-		server_restart
+		apache_restart
 		mysql_restart
 		postgres_restart
 		end_session
@@ -245,11 +338,18 @@ case "$ARGV" in
             use55       -- Switch to php 5.5
             use56       -- Switch to php 5.6
             php         -- Display which version is linked
+            php [log|tail]
 
     Controlling individual components 
-            postgres  [stop|start|restart]  -- PostgreSQL
-            mysql     [stop|start|restart]  -- MySQL
-            apache    [stop|start|restart]  -- Apache
+            postgres  [stop|start|restart|log|tail|pidfile]  -- PostgreSQL
+            mysql     [stop|start|restart|log|tail|pidfile]  -- MySQL
+            apache    [
+                          stop
+                          start
+                          restart
+                          logs
+                          tail [error|access|deverror|devaccess]
+                      ]  -- Apache
         "
         exit 1;;
 esac
